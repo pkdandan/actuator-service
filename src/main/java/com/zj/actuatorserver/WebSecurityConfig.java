eb0ca85c69d2ed9fe8d44ce27edf4e89af4e7fb7
Robert <robert@aleph-labs.com>
2018-04-16 13:46:46 +0800

    move username and password to property file

--- src/main/java/com/zj/actuatorserver/WebSecurityConfig.java
+++ src/main/java/com/zj/actuatorserver/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package com.zj.actuatorserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Value("${security.user.name}")
+	private String username;
+	@Value("${security.user.password}")
+	private String password;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser(username).password(password).roles("ADMIN");
 	}
 	
 //	@Override
